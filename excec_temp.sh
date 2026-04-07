e8f7fcfcb613ed464cb28ab5b03e6607b5010207
rahadiana nugraha <rahadiana@users.noreply.github.com>
2026-04-08 02:42:53 +0700

    Update excec_temp.sh

--- excec_temp.sh
+++ excec_temp.sh
@@ -1,5 +1,5 @@
 #opencode run "console.log('jalankan curl -s https://raw.githubusercontent.com/code-yeongyu/oh-my-openagent/refs/heads/dev/docs/guide/installation.md , install semua dan saya tidak mempunyai semua paket berbayar')" -m opencode/minimax-m2.5-free
-opencode run "console.log('kerjakan MARKDOWN_PLAN/2 - start planning.md , cek progres setiap 5 detik , setiap menyelesaikan todos pastikan semua harus lulus tes, kosongkan CHANGELOG.md , lalu buat perubahan di CHANGELOG.md sertakan input dan output test dari masing-masing script')" -m opencode/minimax-m2.5-free
+opencode run "console.log('baca CHANGELOG.md implementasikan setiap plan yang belum dilaksanakan di folder MARKDOWN_PLAN , cek progres setiap 5 detik , setiap menyelesaikan todos pastikan semua harus lulus tes, lalu buat perubahan di CHANGELOG.md')" -m opencode/minimax-m2.5-free
 # rm -rf node_modules
 # rm file_list.txt
 # ls -R > file_list.txt
